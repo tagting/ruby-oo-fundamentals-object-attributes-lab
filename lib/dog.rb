fcf26174ef50efa95a0a214fd729632dbb614d64
ting <ttzhenng@gmail.com>
2020-12-11 14:52:41 -0500

    commit tree ccd4f4c964f986bedb415293f47d6d5a008b64e2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end 
+
 end #Dog Class
